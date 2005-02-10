d8195e7c43d9e1ae93f158cf0df88ca477a0ce5e
Michael Hieke <mghie@users.sourceforge.net>
2005-02-10 12:27:51 +0000

    Compilation for gcc with precompiled headers fixed
    svn path=/trunk/flamerobin/; revision=68

--- src/gui/OptionsDialog.h
+++ src/gui/OptionsDialog.h
@@ -21,18 +21,6 @@
   Contributor(s):
 */
 
-// For compilers that support precompilation, includes "wx/wx.h".
-#include "wx/wxprec.h"
-
-#ifdef __BORLANDC__
-    #pragma hdrstop
-#endif
-
-// for all others, include the necessary headers (this file is usually all you
-// need because it includes almost all "standard" wxWindows headers
-#ifndef WX_PRECOMP
-    #include "wx/wx.h"
-#endif
 //-----------------------------------------------------------------------------
 #ifndef OPTIONSDIALOG_H
 #define OPTIONSDIALOG_H
