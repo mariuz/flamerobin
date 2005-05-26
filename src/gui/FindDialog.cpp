b2631ba5062146d9b08a9dcd93a1b0a800bc5ed8
Milan Babuskov <mbabuskov@users.sourceforge.net>
2005-05-26 19:53:16 +0000

    Added licence header
    svn path=/trunk/flamerobin/; revision=202

--- src/gui/FindDialog.cpp
+++ src/gui/FindDialog.cpp
@@ -1,3 +1,28 @@
+/*
+  The contents of this file are subject to the Initial Developer's Public
+  License Version 1.0 (the "License"); you may not use this file except in
+  compliance with the License. You may obtain a copy of the License here:
+  http://www.flamerobin.org/license.html.
+
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+  License for the specific language governing rights and limitations under
+  the License.
+
+  The Original Code is FlameRobin (TM).
+
+  The Initial Developer of the Original Code is Milan Babuskov.
+
+  Portions created by the original developer
+  are Copyright (C) 2005 Milan Babuskov.
+
+  All Rights Reserved.
+
+  $Id$
+
+  Contributor(s): _____________________
+*/
+
 #include "styleguide.h"
 #include "FindDialog.h"
 //-----------------------------------------------------------------------------
