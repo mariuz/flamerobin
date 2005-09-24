ebdb5e5d7b52c992f5b0140796916353d0c2208b
Michael Hieke <mghie@users.sourceforge.net>
2005-09-24 08:53:54 +0000

    Switched over to new text control class
    svn path=/trunk/flamerobin/; revision=546

--- src/gui/MultilineEnterDialog.cpp
+++ src/gui/MultilineEnterDialog.cpp
@@ -1,79 +1,64 @@
 /*
-The contents of this file are subject to the Initial Developer's Public
-License Version 1.0 (the "License"); you may not use this file except in
-compliance with the License. You may obtain a copy of the License here:
-http://www.flamerobin.org/license.html.
+  The contents of this file are subject to the Initial Developer's Public
+  License Version 1.0 (the "License"); you may not use this file except in
+  compliance with the License. You may obtain a copy of the License here:
+  http://www.flamerobin.org/license.html.
 
-Software distributed under the License is distributed on an "AS IS"
-basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-License for the specific language governing rights and limitations under
-the License.
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+  License for the specific language governing rights and limitations under
+  the License.
 
-The Original Code is FlameRobin (TM).
+  The Original Code is FlameRobin (TM).
 
-The Initial Developer of the Original Code is Milan Babuskov.
+  The Initial Developer of the Original Code is Milan Babuskov.
 
-Portions created by the original developer
-are Copyright (C) 2004 Milan Babuskov.
+  Portions created by the original developer
+  are Copyright (C) 2004 Milan Babuskov.
 
-All Rights Reserved.
+  All Rights Reserved.
 
-$Id$
+  $Id$
 
-Contributor(s): Michael Hieke, Nando Dessena
+  Contributor(s): Michael Hieke, Nando Dessena
 */
 
 // For compilers that support precompilation, includes "wx/wx.h".
 #include "wx/wxprec.h"
 
 #ifdef __BORLANDC__
-#pragma hdrstop
+  #pragma hdrstop
 #endif
 
 // for all others, include the necessary headers (this file is usually all you
 // need because it includes almost all "standard" wxWindows headers
 #ifndef WX_PRECOMP
-#include "wx/wx.h"
+  #include "wx/wx.h"
 #endif
-
-#include <wx/clipbrd.h>
 
 #include "MultilineEnterDialog.h"
 #include "styleguide.h"
 //-----------------------------------------------------------------------------
-bool GetMultilineTextFromUser(const wxString& caption, wxString& value, wxWindow *parent)
+bool GetMultilineTextFromUser(const wxString& caption, wxString& value, 
+    wxWindow* parent)
 {
-    wxString result(value);
-    MultilineEnterDialog med(parent, caption, result);
+    MultilineEnterDialog med(parent, caption);
+    med.setText(value);
     if (wxID_OK != med.ShowModal())
         return false;
     value = med.getText();
     return true;
 }
 //-----------------------------------------------------------------------------
-MultilineEnterDialog::MultilineEnterDialog(wxWindow* parent, const wxString& title, const wxString& initialText)
-    : BaseDialog(parent, -1, title)
+MultilineEnterDialog::MultilineEnterDialog(wxWindow* parent, 
+        const wxString& title)
+    : BaseDialog(parent, wxID_ANY, title)
 {
-	text_ctrl_value = new TextCtrlWithContextMenu(getControlsPanel(), initialText);
-    button_ok = new wxButton(getControlsPanel(), ID_button_ok, _("Save"));
-    button_cancel = new wxButton(getControlsPanel(), ID_button_cancel, _("Cancel"));
-    do_layout();
+	text_ctrl_value = new TextControl(getControlsPanel());
+    button_ok = new wxButton(getControlsPanel(), wxID_OK, _("Save"));
+    button_cancel = new wxButton(getControlsPanel(), wxID_CANCEL, _("Cancel"));
+    layoutControls();
     button_ok->SetDefault();
-}
-//-----------------------------------------------------------------------------
-void MultilineEnterDialog::do_layout()
-{
-    wxBoxSizer* sizerControls = new wxBoxSizer(wxHORIZONTAL);
-    text_ctrl_value->SetSizeHints(200, 100);
-    text_ctrl_value->SetSize(200, 100);
-    sizerControls->Add(text_ctrl_value, 1, wxEXPAND);
-    wxSizer* sizerButtons = styleguide().createButtonSizer(button_ok, button_cancel);
-    layoutSizers(sizerControls, sizerButtons, true);
-}
-//-----------------------------------------------------------------------------
-const wxString MultilineEnterDialog::getName() const
-{
-    return wxT("MultilineEnterDialog");
 }
 //-----------------------------------------------------------------------------
 wxString MultilineEnterDialog::getText() const
@@ -81,95 +66,23 @@ wxString MultilineEnterDialog::getText() const
     return text_ctrl_value->GetText();
 }
 //-----------------------------------------------------------------------------
-TextCtrlWithContextMenu::TextCtrlWithContextMenu(wxWindow* parent, const wxString& initialText)
-	:wxStyledTextCtrl(parent, myId, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER)
+void MultilineEnterDialog::setText(const wxString& text)
 {
-	SetMarginWidth(1, 0);			// turn off the margins
-	SetMarginWidth(0, 0);
-    SetWrapMode(wxSTC_WRAP_WORD);
-
-	SetText(initialText);
+    text_ctrl_value->SetText(text);
 }
 //-----------------------------------------------------------------------------
-BEGIN_EVENT_TABLE(TextCtrlWithContextMenu, wxStyledTextCtrl)
-    EVT_CONTEXT_MENU(TextCtrlWithContextMenu::OnContextMenu)
-	EVT_STC_START_DRAG(TextCtrlWithContextMenu::myId, TextCtrlWithContextMenu::OnStartDrag)
-    EVT_MENU(TextCtrlWithContextMenu::ID_MENU_UNDO,             TextCtrlWithContextMenu::OnMenuUndo)
-    EVT_MENU(TextCtrlWithContextMenu::ID_MENU_REDO,             TextCtrlWithContextMenu::OnMenuRedo)
-    EVT_MENU(TextCtrlWithContextMenu::ID_MENU_CUT,              TextCtrlWithContextMenu::OnMenuCut)
-    EVT_MENU(TextCtrlWithContextMenu::ID_MENU_COPY,             TextCtrlWithContextMenu::OnMenuCopy)
-    EVT_MENU(TextCtrlWithContextMenu::ID_MENU_PASTE,            TextCtrlWithContextMenu::OnMenuPaste)
-    EVT_MENU(TextCtrlWithContextMenu::ID_MENU_DELETE,           TextCtrlWithContextMenu::OnMenuDelete)
-    EVT_MENU(TextCtrlWithContextMenu::ID_MENU_SELECT_ALL,       TextCtrlWithContextMenu::OnMenuSelectAll)
-END_EVENT_TABLE()
-//-----------------------------------------------------------------------------
-// Avoiding the annoying thing that you cannot click inside the selection and have it deselected and have caret there
-void TextCtrlWithContextMenu::OnStartDrag(wxStyledTextEvent& WXUNUSED(event))
+const wxString MultilineEnterDialog::getName() const
 {
-	wxPoint mp = ::wxGetMousePosition();
-    int p = PositionFromPoint(ScreenToClient(mp));
-	SetSelectionStart(p);	// deselect text
-	SetSelectionEnd(p);
+    return wxT("MultilineEnterDialog");
 }
 //-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnContextMenu(wxContextMenuEvent& WXUNUSED(event))
+void MultilineEnterDialog::layoutControls()
 {
-    wxMenu m(0);
-    m.Append(ID_MENU_UNDO, _("&Undo"));
-    m.Append(ID_MENU_REDO, _("&Redo"));
-    m.AppendSeparator();
-    m.Append(ID_MENU_CUT,    _("Cu&t"));
-    m.Append(ID_MENU_COPY,   _("&Copy"));
-    m.Append(ID_MENU_PASTE,  _("&Paste"));
-    m.Append(ID_MENU_DELETE, _("&Delete"));
-    m.AppendSeparator();
-    m.Append(ID_MENU_SELECT_ALL,       _("Select &All"));
-
-	// disable stuff
-	m.Enable(ID_MENU_UNDO, CanUndo());
-	m.Enable(ID_MENU_REDO, CanRedo());
-	if (GetSelectionStart() == GetSelectionEnd())		// nothing is selected
-	{
-		m.Enable(ID_MENU_CUT,              false);
-		m.Enable(ID_MENU_COPY,             false);
-		m.Enable(ID_MENU_DELETE,           false);
-	}
-
-    PopupMenu(&m, ScreenToClient(::wxGetMousePosition()));
-}
-//-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnMenuUndo(wxCommandEvent& WXUNUSED(event))
-{
-	Undo();
-}
-//-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnMenuRedo(wxCommandEvent& WXUNUSED(event))
-{
-	Redo();
-}
-//-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnMenuCut(wxCommandEvent& WXUNUSED(event))
-{
-	Cut();
-}
-//-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnMenuCopy(wxCommandEvent& WXUNUSED(event))
-{
-	Copy();
-}
-//-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnMenuPaste(wxCommandEvent& WXUNUSED(event))
-{
-	Paste();
-}
-//-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnMenuDelete(wxCommandEvent& WXUNUSED(event))
-{
-	Clear();
-}
-//-----------------------------------------------------------------------------
-void TextCtrlWithContextMenu::OnMenuSelectAll(wxCommandEvent& WXUNUSED(event))
-{
-	SelectAll();
+    wxBoxSizer* sizerControls = new wxBoxSizer(wxHORIZONTAL);
+    text_ctrl_value->SetSizeHints(200, 100);
+    text_ctrl_value->SetSize(200, 100);
+    sizerControls->Add(text_ctrl_value, 1, wxEXPAND);
+    wxSizer* sizerButtons = styleguide().createButtonSizer(button_ok, button_cancel);
+    layoutSizers(sizerControls, sizerButtons, true);
 }
 //-----------------------------------------------------------------------------
--- src/gui/MultilineEnterDialog.h
+++ src/gui/MultilineEnterDialog.h
@@ -1,81 +1,55 @@
 /*
-The contents of this file are subject to the Initial Developer's Public
-License Version 1.0 (the "License"); you may not use this file except in
-compliance with the License. You may obtain a copy of the License here:
-http://www.flamerobin.org/license.html.
+  The contents of this file are subject to the Initial Developer's Public
+  License Version 1.0 (the "License"); you may not use this file except in
+  compliance with the License. You may obtain a copy of the License here:
+  http://www.flamerobin.org/license.html.
 
-Software distributed under the License is distributed on an "AS IS"
-basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-License for the specific language governing rights and limitations under
-the License.
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+  License for the specific language governing rights and limitations under
+  the License.
 
-The Original Code is FlameRobin (TM).
+  The Original Code is FlameRobin (TM).
 
-The Initial Developer of the Original Code is Milan Babuskov.
+  The Initial Developer of the Original Code is Milan Babuskov.
 
-Portions created by the original developer
-are Copyright (C) 2004 Milan Babuskov.
+  Portions created by the original developer
+  are Copyright (C) 2004 Milan Babuskov.
 
-All Rights Reserved.
+  All Rights Reserved.
 
-$Id$
+  $Id$
 
-Contributor(s): Michael Hieke, Nando Dessena
+  Contributor(s): Michael Hieke, Nando Dessena
 */
 
+//-----------------------------------------------------------------------------
 #ifndef MULTILINEENTERDIALOG_H
 #define MULTILINEENTERDIALOG_H
-//-----------------------------------------------------------------------------
+
 #include <wx/wx.h>
-#include <wx/stc/stc.h>
 
 #include "gui/BaseDialog.h"
+#include "gui/controls/TextControl.h"
 //-----------------------------------------------------------------------------
-bool GetMultilineTextFromUser(const wxString& caption, wxString& value, wxWindow* parent=0);
+bool GetMultilineTextFromUser(const wxString& caption, wxString& value, 
+    wxWindow* parent = 0);
 //-----------------------------------------------------------------------------
-class TextCtrlWithContextMenu: public wxStyledTextCtrl
-{
-private:
-	enum { myId = 500 };
-public:
-	TextCtrlWithContextMenu(wxWindow* parent, const wxString& initialText = wxEmptyString);
-
-	// standard context menu for edit controls. Needed since wxTextCtrl on GTK1 does not provide it
-	enum { 
-		ID_MENU_UNDO = 300, ID_MENU_REDO, ID_MENU_CUT, ID_MENU_COPY, ID_MENU_PASTE, ID_MENU_DELETE, ID_MENU_SELECT_ALL
-	};
-	void OnStartDrag(wxStyledTextEvent& event);
-	void OnContextMenu(wxContextMenuEvent& event);
-	void OnMenuUndo(wxCommandEvent& event);
-	void OnMenuRedo(wxCommandEvent& event);
-	void OnMenuCut(wxCommandEvent& event);
-	void OnMenuCopy(wxCommandEvent& event);
-	void OnMenuPaste(wxCommandEvent& event);
-	void OnMenuDelete(wxCommandEvent& event);
-	void OnMenuSelectAll(wxCommandEvent& event);
-    DECLARE_EVENT_TABLE()
-};
-//-----------------------------------------------------------------------------
-//! normally you shouldn't need to create objects of this class, just use the GetMultilineTextFromUser function
+//! normally you shouldn't need to create objects of this class, just use
+//  the GetMultilineTextFromUser() function
 class MultilineEnterDialog: public BaseDialog {
-public:
-    enum {
-        ID_button_ok = wxID_OK,
-        ID_button_cancel = wxID_CANCEL
-    };
-	
-    wxString getText() const;
-    MultilineEnterDialog(wxWindow* parent, const wxString& title, const wxString& initialText);
-
 private:
-    void do_layout();
-    void set_properties();
-
-protected:
-    TextCtrlWithContextMenu* text_ctrl_value;
+    TextControl* text_ctrl_value;
     wxButton* button_ok;
     wxButton* button_cancel;
+    void layoutControls();
+protected:
     virtual const wxString getName() const;
+public:
+    MultilineEnterDialog(wxWindow* parent, const wxString& title);
+        
+    wxString getText() const;
+    void setText(const wxString& text);
 };
 //-----------------------------------------------------------------------------
-#endif // MULTILINEENTERDIALOG_H
+#endif
