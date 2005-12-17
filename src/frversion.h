bb218b416a940257b346bdb1f31272b771178fd1
Milan Babuskov <mbabuskov@users.sourceforge.net>
2005-12-17 20:49:10 +0000

    Moved version to 0.5.0 (in advance)
    svn path=/trunk/flamerobin/; revision=745

--- src/frversion.h
+++ src/frversion.h
@@ -1,5 +1,5 @@
 // $Id$
 
 #define FR_VERSION_MAJOR          0
-#define FR_VERSION_MINOR          4
-#define FR_VERSION_RLS            8
+#define FR_VERSION_MINOR          5
+#define FR_VERSION_RLS            0
