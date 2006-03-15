6f3e9ec219c7d24c56a9471a1ecb4c202b9c9d3c
Michael Hieke <mghie@users.sourceforge.net>
2006-03-15 17:19:16 +0000

    * Removed commented out code, used empty() method for consistency
    svn path=/trunk/flamerobin/; revision=895

--- src/ugly.cpp
+++ src/ugly.cpp
@@ -36,7 +36,6 @@
     #include "wx/wx.h"
 #endif
 
-#include <wx/wx.h>
 #include <wx/encconv.h>
 
 #include "ugly.h"
@@ -47,45 +46,17 @@
 //! converts wxString to std::string
 std::string wx2std(const wxString& input)
 {
-    if (input.IsEmpty())
+    if (input.empty())
         return "";
     return std::string(input.mb_str(*wxConvCurrent));
-/*
-#if (wxUSE_UNICODE)
-	size_t size = input.Length() + 1;
-	char* buffer = new char[size];
-	wxEncodingConverter wxec;
-	wxec.Init(wxFONTENCODING_UNICODE, wxFONTENCODING_ISO8859_1);
-	wxec.Convert(input.wc_str(), buffer);
-	std::string temp(buffer);
-	delete buffer;
-	return temp;
-#else
-	return std::string(input.c_str());
-#endif
-*/
 }
 //-----------------------------------------------------------------------------
 //! converts std:string to wxString
 wxString std2wx(std::string input)
 {
-    if (input == "")
+    if (input.empty())
         return wxEmptyString;
     return wxString(input.c_str(), *wxConvCurrent);
-/*
-#if (wxUSE_UNICODE)
-	size_t size = input.size() + 1;
-	wchar_t *buffer = new wchar_t[size*4];	// to make sure if we have 32-bit characters
-	wxEncodingConverter wxec;
-	wxec.Init(wxFONTENCODING_ISO8859_1, wxFONTENCODING_UNICODE);
-	wxec.Convert(input.c_str(), buffer);
-	wxString temp(buffer);
-	delete buffer;
-	return temp;
-#else
-	return wxString(input.c_str());
-#endif
-*/
 }
 //-----------------------------------------------------------------------------
 //! return wxString for comparison, used to limit features to certain platforms
