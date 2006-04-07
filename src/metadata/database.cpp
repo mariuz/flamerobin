8888526bfa8ac440ac0b5dd13488b39a57c2cfb7
Milan Babuskov <mbabuskov@users.sourceforge.net>
2006-04-07 08:05:23 +0000

    Copy encrypt-password flag in copy-ctor
    svn path=/trunk/flamerobin/; revision=985

--- src/metadata/database.cpp
+++ src/metadata/database.cpp
@@ -192,7 +192,7 @@ Database::Database(const Database& rhs)
     functionsM(rhs.functionsM), generatorsM(rhs.generatorsM),
     proceduresM(rhs.proceduresM), rolesM(rhs.rolesM), tablesM(rhs.tablesM),
     triggersM(rhs.triggersM), viewsM(rhs.viewsM), collationsM(rhs.collationsM),
-    idM(rhs.idM)
+    idM(rhs.idM), storeEncryptedPasswordM(rhs.storeEncryptedPasswordM)
 {
     if (rhs.connectionCredentialsM)
         connectionCredentialsM = new Credentials(*connectionCredentialsM);
