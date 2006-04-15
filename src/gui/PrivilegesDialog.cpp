75f3ea38a1190a1fec551b45e32f16c3e6ae3e3b
Milan Babuskov <mbabuskov@users.sourceforge.net>
2006-04-15 08:43:23 +0000

    Removed single border of panel. Reformatted the code to 80 char line width
    svn path=/trunk/flamerobin/; revision=1031

--- src/gui/PrivilegesDialog.cpp
+++ src/gui/PrivilegesDialog.cpp
@@ -56,7 +56,7 @@ PrivilegesDialog::PrivilegesDialog(wxWindow *parent, MetadataItem *object,
 
     wxBoxSizer *mainSizer;
     mainSizer = new wxBoxSizer(wxVERTICAL);
-    mainPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+    mainPanel = new wxPanel(this);
 
     wxBoxSizer *innerSizer;
     innerSizer = new wxBoxSizer(wxVERTICAL);
@@ -64,116 +64,134 @@ PrivilegesDialog::PrivilegesDialog(wxWindow *parent, MetadataItem *object,
     topSizer = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *topLeftSizer;
     topLeftSizer = new wxBoxSizer(wxVERTICAL);
-    {
-    wxString __choices[] = { _("Grant"), _("Revoke") };
-    int __nchoices = sizeof(__choices) / sizeof(wxString);
-    radiobox_action = new wxRadioBox(mainPanel, ID_radiobox_action, _("Action"), wxDefaultPosition, wxDefaultSize, __nchoices, __choices, 1, wxRA_SPECIFY_COLS);
-    }
-
+    wxString choices1[] = { _("Grant"), _("Revoke") };
+    int nchoices1 = sizeof(choices1) / sizeof(wxString);
+    radiobox_action = new wxRadioBox(mainPanel, ID_radiobox_action,
+        _("Action"), wxDefaultPosition, wxDefaultSize, nchoices1, choices1,
+        1, wxRA_SPECIFY_COLS);
     topLeftSizer->Add(radiobox_action, 0, wxALL|wxEXPAND, 5);
-    checkbox_grant_option = new wxCheckBox(mainPanel, ID_checkbox, _("Grant/Admin option"), wxDefaultPosition, wxDefaultSize, 0);
 
+    checkbox_grant_option = new wxCheckBox(mainPanel, ID_checkbox,
+        _("Grant/Admin option"));
     topLeftSizer->Add(checkbox_grant_option, 0, wxALL|wxEXPAND, 5);
-    granteePanel = new wxPanel(mainPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSIMPLE_BORDER|wxTAB_TRAVERSAL);
 
-    wxBoxSizer *granteeSizer;
-    granteeSizer = new wxBoxSizer(wxVERTICAL);
-    m_staticText3 = new wxStaticText(granteePanel, wxID_ANY, _("GRANTEE"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
-    wxFont font(m_staticText3->GetFont());
-    font.SetWeight(wxBOLD);
-    m_staticText3->SetFont(font);
-
-    granteeSizer->Add(m_staticText3, 0, wxALL|wxEXPAND, 5);
+    granteePanel = new wxPanel(mainPanel);
+    wxStaticBoxSizer *granteeSizer = new wxStaticBoxSizer(wxVERTICAL,
+        granteePanel, _("Grantee"));
     wxFlexGridSizer *fgSizer3;
     fgSizer3 = new wxFlexGridSizer(2, 2, 0, 0);
     fgSizer3->AddGrowableCol(1);
-    radiobtn_user = new wxRadioButton(granteePanel, ID_radiobtn, _("User/Role"), wxDefaultPosition, wxDefaultSize, 0);
 
+    radiobtn_user = new wxRadioButton(granteePanel, ID_radiobtn,
+        _("User/Role"));
     fgSizer3->Add(radiobtn_user, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
-    textctrl_user = new wxTextCtrl(granteePanel, ID_textctrl_user, wxT("PUBLIC"), wxDefaultPosition, wxDefaultSize, 0);
-
+    textctrl_user = new wxTextCtrl(granteePanel, ID_textctrl_user,
+        wxT("PUBLIC"));
     fgSizer3->Add(textctrl_user, 0, wxALL|wxEXPAND, 5);
-    radiobtn_trigger = new wxRadioButton(granteePanel, ID_radiobtn, _("Trigger"), wxDefaultPosition, wxDefaultSize, 0);
 
+    radiobtn_trigger = new wxRadioButton(granteePanel, ID_radiobtn,
+        _("Trigger"));
     fgSizer3->Add(radiobtn_trigger, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
     {
         MetadataCollection<Trigger>* tc = databaseM->getCollection<Trigger>();
         wxArrayString choices;
-        for (MetadataCollection<Trigger>::iterator it = tc->begin(); it != tc->end(); ++it)
+        for (MetadataCollection<Trigger>::iterator it = tc->begin();
+            it != tc->end(); ++it)
+        {
             choices.Add((*it).getName_());
-        choice_trigger = new wxChoice(granteePanel, ID_choice, wxDefaultPosition, wxDefaultSize, choices);
+        }
+        choice_trigger = new wxChoice(granteePanel, ID_choice,
+            wxDefaultPosition, wxDefaultSize, choices);
         if (!choices.IsEmpty())
             choice_trigger->SetSelection(0);
     }
     choice_trigger->Enable(false);
 
     fgSizer3->Add(choice_trigger, 0, wxALL|wxEXPAND, 5);
-    radiobtn_procedure = new wxRadioButton(granteePanel, ID_radiobtn, _("Procedure"), wxDefaultPosition, wxDefaultSize, 0);
-
+    radiobtn_procedure = new wxRadioButton(granteePanel, ID_radiobtn,
+         _("Procedure"));
     fgSizer3->Add(radiobtn_procedure, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
     {
-        MetadataCollection<Procedure>* tc = databaseM->getCollection<Procedure>();
+        MetadataCollection<Procedure>* tc =
+            databaseM->getCollection<Procedure>();
         wxArrayString choices;
-        for (MetadataCollection<Procedure>::iterator it = tc->begin(); it != tc->end(); ++it)
+        for (MetadataCollection<Procedure>::iterator it = tc->begin();
+            it != tc->end(); ++it)
+        {
             choices.Add((*it).getName_());
-        choice_procedure = new wxChoice(granteePanel, ID_choice, wxDefaultPosition, wxDefaultSize, choices);
+        }
+        choice_procedure = new wxChoice(granteePanel, ID_choice,
+            wxDefaultPosition, wxDefaultSize, choices);
         if (!choices.IsEmpty())
             choice_procedure->SetSelection(0);
     }
     choice_procedure->Enable(false);
-
     fgSizer3->Add(choice_procedure, 0, wxALL|wxEXPAND, 5);
-    radiobtn_view = new wxRadioButton(granteePanel, ID_radiobtn, _("View"), wxDefaultPosition, wxDefaultSize, 0);
 
+    radiobtn_view = new wxRadioButton(granteePanel, ID_radiobtn, _("View"));
     fgSizer3->Add(radiobtn_view, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
     {
         MetadataCollection<View>* tc = databaseM->getCollection<View>();
         wxArrayString choices;
-        for (MetadataCollection<View>::iterator it = tc->begin(); it != tc->end(); ++it)
+        for (MetadataCollection<View>::iterator it = tc->begin();
+            it != tc->end(); ++it)
+        {
             choices.Add((*it).getName_());
-        choice_view = new wxChoice(granteePanel, ID_choice, wxDefaultPosition, wxDefaultSize, choices);
+        }
+        choice_view = new wxChoice(granteePanel, ID_choice, wxDefaultPosition,
+            wxDefaultSize, choices);
         if (!choices.IsEmpty())
             choice_view->SetSelection(0);
     }
     choice_view->Enable(false);
-
     fgSizer3->Add(choice_view, 0, wxALL|wxEXPAND, 5);
+
     granteeSizer->Add(fgSizer3, 1, wxEXPAND, 5);
     granteePanel->SetSizer(granteeSizer);
     topLeftSizer->Add(granteePanel, 1, wxEXPAND | wxALL, 5);
     topSizer->Add(topLeftSizer, 0, wxEXPAND, 0);
-    privilegesPanel = new wxPanel(mainPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSIMPLE_BORDER|wxTAB_TRAVERSAL);
-
+    topSizer->Add(12, 12, 0, wxALL, 5); // separate left and right parts
+    privilegesPanel = new wxPanel(mainPanel);
     wxBoxSizer *privilegesSizer;
     privilegesSizer = new wxBoxSizer(wxVERTICAL);
-    m_staticText2 = new wxStaticText(privilegesPanel, wxID_ANY, _("PRIVILEGES"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
+    m_staticText2 = new wxStaticText(privilegesPanel, wxID_ANY,
+        _("Privileges"));
     wxFont font2(m_staticText2->GetFont());
     font2.SetWeight(wxBOLD);
-    m_staticText2->SetFont(font);
+    m_staticText2->SetFont(font2);
+    privilegesSizer->Add(m_staticText2, 0, wxALL|wxEXPAND, 4);
 
-    privilegesSizer->Add(m_staticText2, 0, wxALL|wxEXPAND, 5);
     wxBoxSizer *relationPrivilegesSizer;
     relationPrivilegesSizer = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *relPrivLeftSizer;
     relPrivLeftSizer = new wxBoxSizer(wxVERTICAL);
-    checkbox_all = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("All"), wxDefaultPosition, wxDefaultSize, 0);
-
+    checkbox_all = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("All"));
     relPrivLeftSizer->Add(checkbox_all, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
-    checkbox_select = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("Select"), wxDefaultPosition, wxDefaultSize, 0);
 
-    relPrivLeftSizer->Add(checkbox_select, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
-    checkbox_insert = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("Insert"), wxDefaultPosition, wxDefaultSize, 0);
+    checkbox_select = new wxCheckBox(privilegesPanel, ID_checkbox,
+        wxT("Select"));
+    relPrivLeftSizer->Add(checkbox_select, 0,
+        wxALL|wxALIGN_CENTER_VERTICAL, 4);
 
-    relPrivLeftSizer->Add(checkbox_insert, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
-    checkbox_update = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("Update"), wxDefaultPosition, wxDefaultSize, 0);
+    checkbox_insert = new wxCheckBox(privilegesPanel, ID_checkbox,
+        wxT("Insert"));
+    relPrivLeftSizer->Add(checkbox_insert, 0,
+        wxALL|wxALIGN_CENTER_VERTICAL, 4);
 
-    relPrivLeftSizer->Add(checkbox_update, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
-    checkbox_delete = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("Delete"), wxDefaultPosition, wxDefaultSize, 0);
+    checkbox_update = new wxCheckBox(privilegesPanel, ID_checkbox,
+        wxT("Update"));
+    relPrivLeftSizer->Add(checkbox_update, 0,
+        wxALL|wxALIGN_CENTER_VERTICAL, 4);
 
-    relPrivLeftSizer->Add(checkbox_delete, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
-    checkbox_references = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("References"), wxDefaultPosition, wxDefaultSize, 0);
+    checkbox_delete = new wxCheckBox(privilegesPanel, ID_checkbox,
+        wxT("Delete"));
+    relPrivLeftSizer->Add(checkbox_delete, 0,
+        wxALL|wxALIGN_CENTER_VERTICAL, 4);
 
-    relPrivLeftSizer->Add(checkbox_references, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
+    checkbox_references = new wxCheckBox(privilegesPanel, ID_checkbox,
+        wxT("References"));
+    relPrivLeftSizer->Add(checkbox_references, 0,
+        wxALL|wxALIGN_CENTER_VERTICAL, 4);
     relationPrivilegesSizer->Add(relPrivLeftSizer, 0, wxEXPAND, 0);
     wxBoxSizer *relPrivRightSizer;
     relPrivRightSizer = new wxBoxSizer(wxVERTICAL);
@@ -182,20 +200,23 @@ PrivilegesDialog::PrivilegesDialog(wxWindow *parent, MetadataItem *object,
         wxArrayString choices;
         MetadataCollection<Table>* tt = databaseM->getCollection<Table>();
         size_t relation_to_select = 0;
-        for (MetadataCollection<Table>::iterator it = tt->begin(); it != tt->end(); ++it)
+        for (MetadataCollection<Table>::iterator it = tt->begin();
+            it != tt->end(); ++it)
         {
             if (dynamic_cast<Table *>(object) == &(*it))
                 relation_to_select = choices.GetCount();
             choices.Add((*it).getName_());
         }
         MetadataCollection<View>* tv = databaseM->getCollection<View>();
-        for (MetadataCollection<View>::iterator it = tv->begin(); it != tv->end(); ++it)
+        for (MetadataCollection<View>::iterator it = tv->begin();
+            it != tv->end(); ++it)
         {
             if (dynamic_cast<View *>(object) == &(*it))
                 relation_to_select = choices.GetCount();
             choices.Add((*it).getName_());
         }
-        choice_relations = new wxChoice(privilegesPanel, ID_choice, wxDefaultPosition, wxDefaultSize, choices);
+        choice_relations = new wxChoice(privilegesPanel, ID_choice,
+            wxDefaultPosition, wxDefaultSize, choices);
         if (!choices.IsEmpty())
             choice_relations->SetSelection(relation_to_select);
     }
@@ -209,85 +230,93 @@ PrivilegesDialog::PrivilegesDialog(wxWindow *parent, MetadataItem *object,
     wxFlexGridSizer *fgSizer2;
     fgSizer2 = new wxFlexGridSizer(2, 2, 0, 0);
     fgSizer2->AddGrowableCol(1);
-    checkbox_execute = new wxCheckBox(privilegesPanel, ID_checkbox,
-        wxT("Execute"), wxDefaultPosition, wxDefaultSize, 0);
 
+    checkbox_execute = new wxCheckBox(privilegesPanel, ID_checkbox,
+        wxT("Execute"));
     fgSizer2->Add(checkbox_execute, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
     {
-        MetadataCollection<Procedure>* tc = databaseM->getCollection<Procedure>();
+        MetadataCollection<Procedure>* tc =
+            databaseM->getCollection<Procedure>();
         wxArrayString choices;
         size_t to_select = 0;
-        for (MetadataCollection<Procedure>::iterator it = tc->begin(); it != tc->end(); ++it)
+        for (MetadataCollection<Procedure>::iterator it = tc->begin();
+            it != tc->end(); ++it)
         {
             if (dynamic_cast<Procedure *>(object) == &(*it))
                 to_select = choices.GetCount();
             choices.Add((*it).getName_());
         }
-        choice_execute = new wxChoice(privilegesPanel, ID_choice, wxDefaultPosition, wxDefaultSize, choices);
+        choice_execute = new wxChoice(privilegesPanel, ID_choice,
+            wxDefaultPosition, wxDefaultSize, choices);
         if (!choices.IsEmpty())
             choice_execute->SetSelection(to_select);
     }
     choice_execute->Enable(false);
-
     fgSizer2->Add(choice_execute, 0, wxEXPAND|wxALL, 5);
-    checkbox_memberof = new wxCheckBox(privilegesPanel, ID_checkbox, wxT("Member of"), wxDefaultPosition, wxDefaultSize, 0);
 
+    checkbox_memberof = new wxCheckBox(privilegesPanel, ID_checkbox,
+        wxT("Member of"));
     fgSizer2->Add(checkbox_memberof, 0, wxALL|wxALIGN_CENTER_VERTICAL, 4);
     {
         MetadataCollection<Role>* tc = databaseM->getCollection<Role>();
         wxArrayString choices;
         size_t to_select = 0;
-        for (MetadataCollection<Role>::iterator it = tc->begin(); it != tc->end(); ++it)
+        for (MetadataCollection<Role>::iterator it = tc->begin();
+            it != tc->end(); ++it)
         {
             if (dynamic_cast<Role *>(object) == &(*it))
                 to_select = choices.GetCount();
             choices.Add((*it).getName_());
         }
-        choice_memberof = new wxChoice(privilegesPanel, ID_choice, wxDefaultPosition, wxDefaultSize, choices);
+        choice_memberof = new wxChoice(privilegesPanel, ID_choice,
+            wxDefaultPosition, wxDefaultSize, choices);
         if (!choices.IsEmpty())
             choice_memberof->SetSelection(to_select);
     }
     choice_memberof->Enable(false);
-
     fgSizer2->Add(choice_memberof, 0, wxALL|wxEXPAND, 5);
+
     privilegesSizer->Add(fgSizer2, 1, wxEXPAND, 5);
     privilegesPanel->SetSizer(privilegesSizer);
     topSizer->Add(privilegesPanel, 1, wxEXPAND | wxALL, 5);
     innerSizer->Add(topSizer, 0, wxEXPAND|wxTOP|wxRIGHT|wxLEFT, 10);
+    innerSizer->Add(10, 5, 0, wxALL, 0);
     wxBoxSizer *previewSqlSizer;
     previewSqlSizer = new wxBoxSizer(wxHORIZONTAL);
-    label_sql = new wxStaticText(mainPanel, wxID_ANY, wxT("SQL"), wxDefaultPosition, wxDefaultSize, 0);
-
+    label_sql = new wxStaticText(mainPanel, wxID_ANY, wxT("SQL"));
     previewSqlSizer->Add(label_sql, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
-    textbox_current_sql = new wxTextCtrl(mainPanel, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxTE_READONLY);
-    textbox_current_sql->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_BTNFACE));
+    textbox_current_sql = new wxTextCtrl(mainPanel, wxID_ANY, wxT(""),
+        wxDefaultPosition, wxDefaultSize, wxTE_READONLY);
+    textbox_current_sql->SetBackgroundColour(
+        wxSystemSettings::GetColour(wxSYS_COLOUR_BTNFACE));
     textbox_current_sql->Enable(false);
-
-    previewSqlSizer->Add(textbox_current_sql, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
-    button_add = new wxButton(mainPanel, ID_button_add, _("&Add to list"), wxDefaultPosition, wxDefaultSize, 0);
-
+    previewSqlSizer->Add(textbox_current_sql, 1,
+        wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    button_add = new wxButton(mainPanel, ID_button_add, _("&Add to list"));
     previewSqlSizer->Add(button_add, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
     innerSizer->Add(previewSqlSizer, 0, wxEXPAND|wxRIGHT|wxLEFT, 10);
-    m_staticline2 = new wxStaticLine(mainPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL);
 
+    m_staticline2 = new wxStaticLine(mainPanel, wxID_ANY, wxDefaultPosition,
+        wxDefaultSize, wxLI_HORIZONTAL);
     innerSizer->Add(m_staticline2, 0, wxALL|wxEXPAND, 6);
-    label_statements = new wxStaticText(mainPanel, wxID_ANY, _("List of SQL statements to execute"), wxDefaultPosition, wxDefaultSize, 0);
 
-    innerSizer->Add(label_statements, 0, wxALIGN_BOTTOM|wxTOP|wxLEFT|wxEXPAND, 10);
-    listbox_statements = new wxListBox(mainPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, 0, 0);
-
+    label_statements = new wxStaticText(mainPanel, wxID_ANY,
+        _("List of SQL statements to execute"));
+    innerSizer->Add(label_statements, 0,
+        wxALIGN_BOTTOM|wxTOP|wxLEFT|wxEXPAND, 10);
+    listbox_statements = new wxListBox(mainPanel, wxID_ANY, wxDefaultPosition,
+        wxDefaultSize, 0, 0, 0);
     innerSizer->Add(listbox_statements, 1, wxEXPAND|wxRIGHT|wxLEFT, 10);
+
     wxBoxSizer *sizer_buttons;
     sizer_buttons = new wxBoxSizer(wxHORIZONTAL);
-    button_remove = new wxButton(mainPanel, ID_button_remove, _("Remove selected"), wxDefaultPosition, wxDefaultSize, 0);
-
+    button_remove = new wxButton(mainPanel, ID_button_remove,
+        _("Remove selected"), wxDefaultPosition, wxDefaultSize, 0);
     sizer_buttons->Add(button_remove, 0, wxALL, 5);
     sizer_buttons->Add(2, 2, 1, wxALL, 0);
-    button_execute = new wxButton(mainPanel, wxID_OK, _("Execute all"), wxDefaultPosition, wxDefaultSize, 0);
-
+    button_execute = new wxButton(mainPanel, wxID_OK, _("Execute all"));
     sizer_buttons->Add(button_execute, 0, wxALL, 5);
-    button_close = new wxButton(mainPanel, wxID_CANCEL, _("Close"), wxDefaultPosition, wxDefaultSize, 0);
-
+    button_close = new wxButton(mainPanel, wxID_CANCEL, _("Close"));
     sizer_buttons->Add(button_close, 0, wxALL, 5);
     innerSizer->Add(sizer_buttons, 0, wxEXPAND|wxBOTTOM|wxRIGHT|wxLEFT, 5);
     mainPanel->SetSizer(innerSizer);
