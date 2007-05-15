c15d06cbf1b68b2bce69f5671d09696cb653b97c
Michael Hieke <mghie@users.sourceforge.net>
2007-05-15 15:02:45 +0000

    * Use local charset instead of UTF-8 for fr_databases.conf for Ansi build (otherwise all non-ASCII chars for names etc. are lost)
    svn path=/trunk/flamerobin/; revision=1290

--- src/metadata/root.cpp
+++ src/metadata/root.cpp
@@ -270,6 +270,9 @@ bool Root::save()
         wxMkdir(dir);
 
     wxXmlDocument doc;
+#if !wxUSE_UNICODE
+    doc.SetFileEncoding(getHtmlCharset());
+#endif
     wxXmlNode* rn = new wxXmlNode(wxXML_ELEMENT_NODE, wxT("root"));
     doc.SetRoot(rn);
 
