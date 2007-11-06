ca277ad1dd9b8714157aee05ffa70af0a4352fef
Michael Hieke <mghie@users.sourceforge.net>
2007-11-06 15:49:21 +0000

    * Location of frsvnrev.h file moved back to src, but added to SVN ignore list to keep it out of the repository
    svn path=/trunk/flamerobin/; revision=1523

--- update-svn-rev.cmd
+++ update-svn-rev.cmd
@@ -4,12 +4,14 @@ rem $Id$
 
 setlocal
 
+set SVNREVFILE=src/frsvnrev.h
+
 rem execute svn info, extract "Revision: 1234" line, and take SVN rev from there
 for /F " usebackq tokens=1,2 delims=: " %%i in (`svn info`) do set key=%%i&set value=%%j&call :read-svn-rev
 rem @echo SVN revision "%SVNREV%"
 
 rem extract "#define FR_VERSION_SVN 1234" line, and take header SVN rev from there
-for /F " usebackq tokens=2,3 " %%i in (frsvnrev.h) do set name=%%i&set value=%%j&call :read-header-rev
+for /F " usebackq tokens=2,3 " %%i in (%SVNREVFILE%) do set name=%%i&set value=%%j&call :read-header-rev
 rem @echo Header svn revision "%HEADERREV%"
 
 rem check for valid SVN rev
@@ -17,15 +19,15 @@ if "%SVNREV%" EQU "" goto :no-svn-ref
 rem don't write file if SVN ref is equal
 if "%HEADERREV%" EQU "%SVNREV%" goto :EOF
 
-@echo Writing svn revision %SVNREV% to frsvnrev.h
-@echo #define FR_VERSION_SVN %SVNREV% > frsvnrev.h
+@echo Writing svn revision %SVNREV% to %SVNREVFILE%
+@echo #define FR_VERSION_SVN %SVNREV% > %SVNREVFILE%
 goto :EOF
 
 :no-svn-ref
 rem don't write file if SVN ref is already unset
 if "%HEADERREV%" EQU "" goto :EOF
-@echo Deleting svn revision from frsvnrev.h
-@echo #undef FR_VERSION_SVN > frsvnrev.h
+@echo Deleting svn revision from %SVNREVFILE%
+@echo #undef FR_VERSION_SVN > %SVNREVFILE%
 goto :EOF
 
 endlocal
--- update-svn-rev.sh
+++ update-svn-rev.sh
@@ -1,24 +1,26 @@
 #/bin/sh
 
 FRDIR=`dirname $0`
+SVNREVFILE=src/svnrev.h
+
 if test -d $FRDIR/.svn ; then
   ACTVERSION=`svn info $FRDIR | awk '/Revision/{print $2}'`
 else
   ACTVERSION=""; 
 fi
 
-if test -f frsvnrev.h ; then
-  HEADERVERSION=`awk '/FR_VERSION_SVN/{print $3}' frsvnrev.h`
+if test -f $SVNREVFILE ; then
+  HEADERVERSION=`awk '/FR_VERSION_SVN/{print $3}' $SVNREVFILE`
 else
   HEADERVERSION="";
 fi
 
 if test -n $ACTVERSION ; then
   if test -z $HEADERVERSION || test $ACTVERSION -gt $HEADERVERSION ; then
-    echo "Writing svn revision $ACTVERSION to frsvnrev.h"
-    echo "#define FR_VERSION_SVN $ACTVERSION" > frsvnrev.h;
+    echo "Writing svn revision $ACTVERSION to $SVNREVFILE"
+    echo "#define FR_VERSION_SVN $ACTVERSION" > $SVNREVFILE;
   fi
 elif test -n $HEADERVERSION ; then
-  echo "Deleting svn revision from frsvnrev.h"
-  echo "#undef FR_VERSION_SVN" > frsvnrev.h
+  echo "Deleting svn revision from $SVNREVFILE"
+  echo "#undef FR_VERSION_SVN" > $SVNREVFILE
 fi
