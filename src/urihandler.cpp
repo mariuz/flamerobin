d65fd88a3caeaf706691e8840deb11060e1981ea
Marius Popa <mapopa@users.sourceforge.net>
2008-02-02 21:15:11 +0000

    updated the copyright year
    svn path=/trunk/flamerobin/; revision=1629

--- src/urihandler.cpp
+++ src/urihandler.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2004, 2005, 2006 The FlameRobin Development Team
+Copyright (c) 2004-2008 The FlameRobin Development Team
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
