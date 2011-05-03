93b14c7e8c903c23ea794983f5b421a3794bcb85
Michael Hieke <mghie@users.sourceforge.net>
2011-05-03 18:34:59 +0000

    * Fix for "Select ... from ..." template command: selected columns were ignored due to wrong config key in the template
    svn path=/trunk/flamerobin/; revision=2113

--- sql-templates/select.sql
+++ sql-templates/select.sql
@@ -1,2 +1,2 @@
-{%--:Uncomment to edit template common params visually.%}{%--:{%confgui:common%}{%abort%}%}{%confgui%}select {%getconf:columnNames%}
+{%--:Uncomment to edit template common params visually.%}{%--:{%confgui:common%}{%abort%}%}{%confgui%}select {%getconf:{%object_path%}/columnNames%}
 from {%object_name%}
