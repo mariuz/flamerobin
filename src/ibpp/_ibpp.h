aec5db2f72649546bf1396cbe384a896ad46eedb
Popa Adrian Marius <mapopa@gmail.com>
2014-12-01 21:33:23 +0200

    use c++ multiline comments

--- src/ibpp/_ibpp.h
+++ src/ibpp/_ibpp.h
@@ -1,32 +1,23 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-//  File    : $Id$
 //  Subject : IBPP internal declarations
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-//  (C) Copyright 2000-2006 T.I.P. Group S.A. and the IBPP Team (www.ibpp.org)
-//
-//  The contents of this file are subject to the IBPP License (the "License");
-//  you may not use this file except in compliance with the License.  You may
-//  obtain a copy of the License at http://www.ibpp.org or in the 'license.txt'
-//  file which must have been distributed along with this file.
-//
-//  This software, distributed under the License, is distributed on an "AS IS"
-//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the
-//  License for the specific language governing rights and limitations
-//  under the License.
-//
-///////////////////////////////////////////////////////////////////////////////
-//
 //  COMMENTS
 //
-//  * 'Internal declarations' means everything used to implement ibpp. This
-//    file and its contents is NOT needed by users of the library. All those
-//    declarations are wrapped in a namespace : 'ibpp_internals'.
-//
-///////////////////////////////////////////////////////////////////////////////
+//  'Internal declarations' means everything used to implement ibpp. This
+//   file and its contents is NOT needed by users of the library. All those
+//   declarations are wrapped in a namespace : 'ibpp_internals'.
 
+/*
+  (C) Copyright 2000-2006 T.I.P. Group S.A. and the IBPP Team (www.ibpp.org)
+
+  The contents of this file are subject to the IBPP License (the "License");
+  you may not use this file except in compliance with the License.  You may
+  obtain a copy of the License at http://www.ibpp.org or in the 'license.txt'
+  file which must have been distributed along with this file.
+
+  This software, distributed under the License, is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the
+  License for the specific language governing rights and limitations
+  under the License.
+*/
 #ifndef __INTERNAL_IBPP_H__
 #define __INTERNAL_IBPP_H__
 
--- src/ibpp/user.cpp
+++ src/ibpp/user.cpp
@@ -1,21 +1,17 @@
-///////////////////////////////////////////////////////////////////////////////
-//
 //	Subject : IBPP, User class implementation
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-//	(C) Copyright 2000-2006 T.I.P. Group S.A. and the IBPP Team (www.ibpp.org)
-//
-//	The contents of this file are subject to the IBPP License (the "License");
-//	you may not use this file except in compliance with the License.  You may
-//	obtain a copy of the License at http://www.ibpp.org or in the 'license.txt'
-//	file which must have been distributed along with this file.
-//
-//	This software, distributed under the License, is distributed on an "AS IS"
-//	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the
-//	License for the specific language governing rights and limitations
-//	under the License.
-//
+/*
+    (C) Copyright 2000-2006 T.I.P. Group S.A. and the IBPP Team (www.ibpp.org)
+
+    The contents of this file are subject to the IBPP License (the "License");
+    you may not use this file except in compliance with the License.  You may
+    obtain a copy of the License at http://www.ibpp.org or in the 'license.txt'
+    file which must have been distributed along with this file.
+
+    This software, distributed under the License, is distributed on an "AS IS"
+    basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the
+    License for the specific language governing rights and limitations
+    under the License.
+*/
 
 #ifdef _MSC_VER
 #pragma warning(disable: 4786 4996)
