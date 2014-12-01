b2c6e3ba03bf710ab37b19ee710e21496daea69c
Popa Adrian Marius <mapopa@gmail.com>
2014-12-01 21:50:18 +0200

    use c++ multiline comments

--- src/ibpp/events.cpp
+++ src/ibpp/events.cpp
@@ -1,23 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-//	File    : $Id$
 //	Subject : IBPP, internal EventsImpl class implementation
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-//	(C) Copyright 2000-2006 T.I.P. Group S.A. and the IBPP Team (www.ibpp.org)
-//
-//	The contents of this file are subject to the IBPP License (the "License");
-//	you may not use this file except in compliance with the License.  You may
-//	obtain a copy of the License at http://www.ibpp.org or in the 'license.txt'
-//	file which must have been distributed along with this file.
-//
-//	This software, distributed under the License, is distributed on an "AS IS"
-//	basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the
-//	License for the specific language governing rights and limitations
-//	under the License.
-//
-///////////////////////////////////////////////////////////////////////////////
 //
 //	COMMENTS
 //
@@ -44,8 +25,20 @@
 //	class, directly based on work by Val Samko.
 //	The whole event handling has then be completely redesigned, based on the old
 //	EPB class to bring up the current IBPP::Events implementation.
-//
-///////////////////////////////////////////////////////////////////////////////
+
+/*
+    (C) Copyright 2000-2006 T.I.P. Group S.A. and the IBPP Team (www.ibpp.org)
+    The contents of this file are subject to the IBPP License (the "License");
+    you may not use this file except in compliance with the License.  You may
+    obtain a copy of the License at http://www.ibpp.org or in the 'license.txt'
+    file which must have been distributed along with this file.
+
+    This software, distributed under the License, is distributed on an "AS IS"
+    basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the
+    License for the specific language governing rights and limitations
+    under the License.
+*/
+
 
 #ifdef _MSC_VER
 #pragma warning(disable: 4786 4996)
