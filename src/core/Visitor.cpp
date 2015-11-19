7fff3f2a3183ed7d2409f87bc9b7793c5f1d4a1c
Popa Adrian Marius <mapopa@reea.net>
2015-11-19 08:14:21 -0800

    add back WX_PRECOMP check and include

--- src/core/Visitor.cpp
+++ src/core/Visitor.cpp
@@ -21,6 +21,15 @@
   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+// For compilers that support precompilation, includes "wx/wx.h".		
+#include "wx/wxprec.h"
+
+// for all others, include the necessary headers (this file is usually all you		
+// need because it includes almost all "standard" wxWindows headers		
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
 #include "Visitor.h"
 
 void Visitor::defaultAction()
