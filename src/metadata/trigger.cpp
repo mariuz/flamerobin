77ecdeffa46054920092ad3f34ebe0df7305c02e
Popa Marius Adrian <mapopa@gmail.com>
2020-08-11 13:50:28 +0300

    Update trigger.cpp
    use kwOR fixes https://github.com/mariuz/flamerobin/issues/53

--- src/metadata/trigger.cpp
+++ src/metadata/trigger.cpp
@@ -214,7 +214,7 @@ wxString Trigger::getAlterSql()
     sb << kwSET << ' ' << kwTERMINATOR << " ^ ;"
         << StatementBuilder::NewLine;
 
-    sb << kwCREATE << ' OR ' << kwALTER << ' ' << kwTRIGGER << ' ' << getQuotedName() << ' ';
+    sb << kwCREATE << ' ' << kwOR << ' ' << kwALTER << ' ' << kwTRIGGER << ' ' << getQuotedName() << ' ';
     if (activeM)
         sb << kwACTIVE;
     else
