0ae5878e195fa057a498c6c6a5185d585c756bee
jdochoa <jose_reyna@yahoo.com>
2022-08-22 17:23:16 -0600

    Refactoring

--- src/gui/BackupRestoreBaseFrame.cpp
+++ src/gui/BackupRestoreBaseFrame.cpp
@@ -43,64 +43,37 @@
 
 BackupRestoreBaseFrame::BackupRestoreBaseFrame(wxWindow* parent,
         DatabasePtr db)
-    : BaseFrame(parent, wxID_ANY, wxEmptyString), databaseM(db), threadM(0)
+    : ThreadBaseFrame(parent, db)
 {
-    wxASSERT(db);
-    db->attachObserver(this, false);
+    //wxASSERT(db);
+    //db->attachObserver(this, false);
 
-    threadMsgTimeMillisM = 0;
+    //threadMsgTimeMillisM = 0;
     verboseMsgsM = true;
 
     // create controls in constructor of descendant class (correct tab order)
-    panel_controls = 0;
+    /*panel_controls = 0;
     label_filename = 0;
     text_ctrl_filename = 0;
     button_browse = 0;
     checkbox_showlog = 0;
     button_start = 0;
-    text_ctrl_log = 0;
+    text_ctrl_log = 0;*/
 
     SetIcon(wxArtProvider::GetIcon(ART_Backup, wxART_FRAME_ICON));
 }
 
 //! implementation details
-void BackupRestoreBaseFrame::addThreadMsg(const wxString msg,
-    bool& notificationNeeded)
-{
-    notificationNeeded = false;
-    wxLongLong millisNow = ::wxGetLocalTimeMillis();
-
-    wxCriticalSectionLocker locker(critsectM);
-    threadMsgsM.Add(msg);
-    // we post no more than 10 events per second to prevent flooding of
-    // the message queue, and to keep the frame responsive for user interaction
-    if ((millisNow - threadMsgTimeMillisM).GetLo() > 100)
-    {
-        threadMsgTimeMillisM = millisNow;
-        notificationNeeded = true;
-    }
-}
-
 void BackupRestoreBaseFrame::cancelBackupRestore()
 {
-    if (threadM != 0)
-    {
-        threadM->Delete();
-        threadM = 0;
-    }
+    cancelThread();
 }
 
-void BackupRestoreBaseFrame::clearLog()
-{
-    msgKindsM.Clear();
-    msgsM.Clear();
-    text_ctrl_log->ClearAll();
-}
 
 bool BackupRestoreBaseFrame::Destroy()
 {
     cancelBackupRestore();
-    return BaseFrame::Destroy();
+    return ThreadBaseFrame::Destroy();
 }
 
 void BackupRestoreBaseFrame::doReadConfigSettings(const wxString& prefix)
@@ -128,10 +101,6 @@ void BackupRestoreBaseFrame::doWriteConfigSettings(const wxString& prefix) const
         text_ctrl_filename->GetValue());
 }
 
-DatabasePtr BackupRestoreBaseFrame::getDatabase() const
-{
-    return databaseM.lock();
-}
 
 const wxString BackupRestoreBaseFrame::getStorageName() const
 {
@@ -140,9 +109,21 @@ const wxString BackupRestoreBaseFrame::getStorageName() const
     return wxEmptyString;
 }
 
-bool BackupRestoreBaseFrame::getThreadRunning() const
+void BackupRestoreBaseFrame::createControls()
 {
-    return threadM != 0;
+    ThreadBaseFrame::createControls();
+
+    label_filename = new wxStaticText(panel_controls, wxID_ANY,
+        _("Backup file:"));
+    text_ctrl_filename = new FileTextControl(panel_controls,
+        ID_text_ctrl_filename, wxEmptyString);
+    button_browse = new wxButton(panel_controls, ID_button_browse, _("..."),
+        wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT);
+
+}
+
+void BackupRestoreBaseFrame::layoutControls()
+{
 }
 
 void BackupRestoreBaseFrame::subjectRemoved(Subject* subject)
@@ -150,52 +131,6 @@ void BackupRestoreBaseFrame::subjectRemoved(Subject* subject)
     DatabasePtr db = getDatabase();
     if (!db || !db->isConnected() || subject == db.get())
         Close();
-}
-
-bool BackupRestoreBaseFrame::startThread(std::unique_ptr<wxThread> thread)
-{
-    wxASSERT(threadM == 0);
-    if (wxTHREAD_NO_ERROR != thread->Create())
-    {
-        ::wxMessageBox(_("Error creating thread!"), _("Error"),
-            wxOK | wxICON_ERROR);
-        return false;
-    }
-    if (wxTHREAD_NO_ERROR != thread->Run())
-    {
-        ::wxMessageBox(_("Error starting thread!"), _("Error"),
-            wxOK | wxICON_ERROR);
-        return false;
-    }
-    threadM = thread.release();
-    return true;
-}
-
-void BackupRestoreBaseFrame::threadOutputMsg(const wxString msg, MsgKind kind)
-{
-    wxString s(msg);
-    switch (kind)
-    {
-    case error_message:
-        s.Prepend("e");
-        break;
-    case important_message:
-        s.Prepend("i");
-        break;
-    case progress_message:
-        s.Prepend("p");
-        break;
-    default:
-        wxASSERT(false);
-        return;
-    }
-    bool doPostMsg = false;
-    addThreadMsg(s, doPostMsg);
-    if (doPostMsg)
-    {
-        wxCommandEvent event(wxEVT_COMMAND_MENU_SELECTED, ID_thread_output);
-        wxPostEvent(this, event);
-    }
 }
 
 void BackupRestoreBaseFrame::update()
@@ -214,28 +149,6 @@ void BackupRestoreBaseFrame::updateControls()
     // completely initialized yet
 }
 
-void BackupRestoreBaseFrame::updateMessages(size_t firstmsg, size_t lastmsg)
-{
-    if (lastmsg > msgsM.GetCount())
-        lastmsg = msgsM.GetCount();
-    for (size_t i = firstmsg; i < lastmsg; i++)
-    {
-        switch ((MsgKind)msgKindsM[i])
-        {
-            case progress_message:
-                if (verboseMsgsM)
-                    text_ctrl_log->logMsg(msgsM[i]);
-                break;
-            case important_message:
-                text_ctrl_log->logImportantMsg(msgsM[i]);
-                break;
-            case error_message:
-                text_ctrl_log->logErrorMsg(msgsM[i]);
-                break;
-        }
-    }
-}
-
 //! event handlers
 BEGIN_EVENT_TABLE(BackupRestoreBaseFrame, BaseFrame)
     EVT_CHECKBOX(BackupRestoreBaseFrame::ID_checkbox_showlog, BackupRestoreBaseFrame::OnVerboseLogChange)
@@ -244,51 +157,6 @@ BEGIN_EVENT_TABLE(BackupRestoreBaseFrame, BaseFrame)
     EVT_TEXT(BackupRestoreBaseFrame::ID_text_ctrl_filename, BackupRestoreBaseFrame::OnSettingsChange)
 END_EVENT_TABLE()
 
-void BackupRestoreBaseFrame::OnSettingsChange(wxCommandEvent& WXUNUSED(event))
-{
-    if (IsShown())
-        updateControls();
-}
-
-void BackupRestoreBaseFrame::OnThreadFinished(wxCommandEvent& event)
-{
-    threadM = 0;
-    OnThreadOutput(event);
-    updateControls();
-}
-
-void BackupRestoreBaseFrame::OnThreadOutput(wxCommandEvent& WXUNUSED(event))
-{
-    wxCriticalSectionLocker locker(critsectM);
-    threadMsgTimeMillisM = ::wxGetLocalTimeMillis();
-
-    size_t first = msgsM.GetCount();
-    for (size_t i = 0; i < threadMsgsM.GetCount(); i++)
-    {
-        wxString s(threadMsgsM[i]);
-        if (s.Length() == 0)
-            continue;
-        switch ((wxChar)s[0])
-        {
-            case 'e':
-                msgKindsM.Add((int)error_message);
-                break;
-            case 'i':
-                msgKindsM.Add((int)important_message);
-                break;
-            case 'p':
-                msgKindsM.Add((int)progress_message);
-                break;
-        }
-        // this depends on server type, so just in case...
-        if (s.Last() != '\n')
-            s.Append('\n');
-        msgsM.Add(s.Mid(1));
-    }
-    threadMsgsM.Clear();
-
-    updateMessages(first, msgsM.GetCount());
-}
 
 void BackupRestoreBaseFrame::OnVerboseLogChange(wxCommandEvent& WXUNUSED(event))
 {
--- src/gui/BackupRestoreBaseFrame.h
+++ src/gui/BackupRestoreBaseFrame.h
@@ -31,58 +31,35 @@
 #include <memory>
 
 #include "core/Observer.h"
-#include "gui/BaseFrame.h"
+#include "gui/ThreadBaseFrame.h"
 #include "metadata/database.h"
 #include "metadata/MetadataClasses.h"
 
 class FileTextControl;
 class LogTextControl;
 
-class BackupRestoreBaseFrame: public BaseFrame, public Observer
+class BackupRestoreBaseFrame: public ThreadBaseFrame//, public Observer
 {
 public:
-    enum MsgKind {
-        progress_message,
-        important_message,
-        error_message
-    };
-
-    enum {
-        ID_thread_output = 500,
-        ID_thread_finished
-    };
 
     // make sure that thread gets deleted
     virtual bool Destroy();
 protected:
-    wxArrayString msgsM;
-    wxArrayInt msgKindsM;
-    bool verboseMsgsM;
-
-    DatabasePtr getDatabase() const;
 
     void cancelBackupRestore();
-    void clearLog();
+    //void clearLog();
     virtual void doReadConfigSettings(const wxString& prefix);
     virtual void doWriteConfigSettings(const wxString& prefix) const;
     virtual const wxString getStorageName() const;
-    // set threadM if thread was successfully created and started,
-    // otherwise delete the thread
-    bool startThread(std::unique_ptr<wxThread> thread);
-    bool getThreadRunning() const;
 
-    void threadOutputMsg(const wxString msg, MsgKind kind);
+    virtual void createControls();
+    virtual void layoutControls();
     virtual void updateControls();
+
     BackupRestoreBaseFrame(wxWindow* parent, DatabasePtr db);
 private:
-    DatabaseWeakPtr databaseM;
-    wxThread* threadM;
 
-    wxCriticalSection critsectM;
-    wxArrayString threadMsgsM;
-    wxLongLong threadMsgTimeMillisM;
-    void addThreadMsg(const wxString msg, bool& notificationNeeded);
-    void updateMessages(size_t firstmsg, size_t lastmsg);
+    //void updateMessages(size_t firstmsg, size_t lastmsg);
 
     // observer stuff
     virtual void subjectRemoved(Subject* subject);
@@ -98,19 +75,16 @@ protected:
         ID_button_start,
     };
 
-    wxPanel* panel_controls;
+    //wxPanel* panel_controls;
     wxStaticText* label_filename;
     FileTextControl* text_ctrl_filename;
     wxButton* button_browse;
-    wxCheckBox* checkbox_showlog;
-    wxButton* button_start;
-    LogTextControl* text_ctrl_log;
-    void setupControls();
+    //wxCheckBox* checkbox_showlog;
+    //wxButton* button_start;
+    //LogTextControl* text_ctrl_log;
+    //void setupControls();
 private:
     // event handling
-    void OnSettingsChange(wxCommandEvent& event);
-    void OnThreadFinished(wxCommandEvent& event);
-    void OnThreadOutput(wxCommandEvent& event);
     void OnVerboseLogChange(wxCommandEvent& event);
 
     DECLARE_EVENT_TABLE()
