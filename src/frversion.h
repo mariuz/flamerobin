74cf500f07f2ea008cddfc121d71d3367443c3e8
arvanus <lucasschatz@gmail.com>
2024-10-13 19:28:16 -0300

    Update frversion.h

--- src/frversion.h
+++ src/frversion.h
@@ -1,6 +1,6 @@
 #define FR_VERSION_MAJOR    0
 #define FR_VERSION_MINOR    9
-#define FR_VERSION_RLS      11
+#define FR_VERSION_RLS      12
 
 // if this file can't be found you need to run the update-revision-info script
 #include "revisioninfo.h"
