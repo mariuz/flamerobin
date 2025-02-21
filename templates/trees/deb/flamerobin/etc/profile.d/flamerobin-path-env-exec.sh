5e2b41d580db3779b93b70808d4c9fd28a55199b
Baldé <baldeuniversel@protonmail.com>
2025-02-21 17:56:44 +0100

    Update the workflow file (**linux-build.yml**) to ensure that, after building the `flamerobin` app, the upstream files [Build/*] are used instead of the copied custom files

--- templates/trees/deb/flamerobin/etc/profile.d/flamerobin-path-env-exec.sh
+++ templates/trees/deb/flamerobin/etc/profile.d/flamerobin-path-env-exec.sh
@@ -3,4 +3,4 @@
 
 
 # Add Firebird and other custom paths to PATH
-export PATH="/opt/firebird/bin:/usr/local/bin:${PATH:-}"
+export PATH="/opt/flamerobin/bin:/opt/firebird/bin:/usr/local/bin:${PATH:-}"
