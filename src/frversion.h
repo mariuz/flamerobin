14db345ade00f6abdf0b470bcfc28780b8db0180
Popa Marius Adrian <mapopa@gmail.com>
2026-04-25 01:50:00 +0300

    Bump version to 1.0.0

--- src/frversion.h
+++ src/frversion.h
@@ -1,6 +1,6 @@
-#define FR_VERSION_MAJOR    0
-#define FR_VERSION_MINOR    9
-#define FR_VERSION_RLS      16
+#define FR_VERSION_MAJOR    1
+#define FR_VERSION_MINOR    0
+#define FR_VERSION_RLS      0
 
 // if this file can't be found you need to run the update-revision-info script
 #include "revisioninfo.h"
