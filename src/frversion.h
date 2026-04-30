72825c869b790f62353e88aeadb183314d869d5e
Popa Marius Adrian <mapopa@gmail.com>
2026-04-30 19:46:07 +0300

    Update release version to 26.4.4
    test release script

--- src/frversion.h
+++ src/frversion.h
@@ -1,6 +1,6 @@
 #define FR_VERSION_MAJOR    26
 #define FR_VERSION_MINOR    4
-#define FR_VERSION_RLS      3
+#define FR_VERSION_RLS      4
 
 // if this file can't be found you need to run the update-revision-info script
 #include "revisioninfo.h"
