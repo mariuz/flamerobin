0a8b4e53da82fa5e246eb6e7cae66d115631c118
Popa Adrian Marius <marius.popa.adrian@reea.net>
2026-05-13 01:33:52 +0300

    Update synchronization script with new charset and length handling fixes.

--- utils/sync-fbcpp.sh
+++ utils/sync-fbcpp.sh
@@ -33,12 +33,22 @@ perl -i -0777 -pe 's/void rollbackRetaining\(\);\n\n\tprivate:/void rollbackReta
 perl -i -0777 -pe 's/using namespace fbcpp::impl;\n\n\nstatic/using namespace fbcpp::impl;\n\n\nTransaction::Transaction(Client& client)\n\t: client{client},\n\t  handle{nullptr},\n\t  state{TransactionState::ROLLED_BACK}\n{\n}\n\n\nstatic/g' "$PATCH_SRC_DIR/Transaction.cpp"
 perl -i -0777 -pe 's/void Transaction::rollbackRetaining\(\)\n\{\n\tassert\(isValid\(\)\);\n\tassert\(state == TransactionState::ACTIVE\);\n\n\tStatusWrapper statusWrapper\{client\};\n\n\thandle->rollbackRetaining\(&statusWrapper\);\n\}/void Transaction::rollbackRetaining()\n{\n\tassert(isValid());\n\tassert(state == TransactionState::ACTIVE);\n\n\tStatusWrapper statusWrapper{client};\n\n\thandle->rollbackRetaining(&statusWrapper);\n}\n\nvoid Transaction::start(Attachment& attachment, const TransactionOptions& options)\n{\n\tassert(!isValid());\n\n\tconst auto master = client.getMaster();\n\tStatusWrapper statusWrapper{client};\n\n\tauto tpbBuilder = buildTpb(master, statusWrapper, options);\n\tconst auto tpbBuffer = tpbBuilder->getBuffer(&statusWrapper);\n\tconst auto tpbBufferLen = tpbBuilder->getBufferLength(&statusWrapper);\n\n\thandle.reset(attachment.getHandle()->startTransaction(&statusWrapper, tpbBufferLen, tpbBuffer));\n\tstate = TransactionState::ACTIVE;\n}/g' "$PATCH_SRC_DIR/Transaction.cpp"
 
-# 3. Statement.h - Add closeCursor()
+# 3. Statement.h - Add closeCursor() and fix truncation check
 perl -i -0777 -pe 's/void free\(\);\n\n\t\t\/\/\/\n\t\t\/\/\/\ @brief Retrieves the textual legacy plan/void free();\n\n\t\tvoid closeCursor();\n\n\t\t\/\/\/\n\t\t\/\/\/\ @brief Retrieves the textual legacy plan/g' "$PATCH_SRC_DIR/Statement.h"
+perl -i -pe 's/if \(value.length\(\) > descriptor.length\)/if (value.length() > descriptor.length - sizeof(std::uint16_t))/g' "$PATCH_SRC_DIR/Statement.h"
 
-# 4. Statement.cpp - Implementations and prepare() decouple
+# 4. Statement.cpp - Implementations, prepare() decouple, and charset/length fix
 perl -i -0777 -pe 's/statementHandle.reset\(attachment.getHandle\(\)->prepare\(&statusWrapper, transaction.getHandle\(\).get\(\),/statementHandle.reset(attachment.getHandle()->prepare(&statusWrapper, nullptr,/g' "$PATCH_SRC_DIR/Statement.cpp"
 perl -i -0777 -pe 's/void Statement::free\(\)\n\{\n\tassert\(isValid\(\)\);\n\n\tif \(resultSetHandle\)\n\t\{\n\t\tresultSetHandle->close\(&statusWrapper\);\n\t\tresultSetHandle.reset\(\);\n\t\}\n\n\tstatementHandle->free\(&statusWrapper\);\n\tstatementHandle.reset\(\);\n\}/void Statement::free()\n{\n\tassert(isValid());\n\n\tcloseCursor();\n\n\tstatementHandle->free(&statusWrapper);\n\tstatementHandle.reset();\n}\n\nvoid Statement::closeCursor()\n{\n\tassert(isValid());\n\n\tif (resultSetHandle)\n\t{\n\t\tresultSetHandle->close(&statusWrapper);\n\t\tresultSetHandle.reset();\n\t}\n}/g' "$PATCH_SRC_DIR/Statement.cpp"
+
+# Add charset 127 and length + 2 fix for TEXT to VARYING conversion
+perl -i -0777 -pe 's/builder->setType\(&statusWrapper, index, SQL_VARYING\);\n\t\t\t\t\tdescriptor.adjustedType = DescriptorAdjustedType::STRING;/builder->setType(&statusWrapper, index, SQL_VARYING);\n\t\t\t\t\tbuilder->setLength(&statusWrapper, index, descriptor.length + 2);\n\t\t\t\t\tbuilder->setCharSet(&statusWrapper, index, 127); \/\/ CS_dynamic\n\t\t\t\t\tdescriptor.adjustedType = DescriptorAdjustedType::STRING;\n\t\t\t\t\tdescriptor.length += 2;\n\t\t\t\t\tdescriptor.charSetId = 127;/g' "$PATCH_SRC_DIR/Statement.cpp"
+
+# Add charset 127 fix for existing VARYING
+perl -i -0777 -pe 's/case DescriptorOriginalType::VARYING:\n\t\t\t\t\tbreak;/case DescriptorOriginalType::VARYING:\n\t\t\t\t\tif (!builder)\n\t\t\t\t\t\tbuilder.reset(metadata->getBuilder(&statusWrapper));\n\n\t\t\t\t\tbuilder->setCharSet(&statusWrapper, index, 127); \/\/ CS_dynamic\n\t\t\t\t\tdescriptor.charSetId = 127;\n\t\t\t\t\tbreak;/g' "$PATCH_SRC_DIR/Statement.cpp"
+
+# Ensure length is re-read in second loop
+perl -i -0777 -pe 's/descriptor.nullOffset = metadata->getNullOffset\(&statusWrapper, index\);/descriptor.nullOffset = metadata->getNullOffset(&statusWrapper, index);\n\t\t\t\tdescriptor.length = metadata->getLength(&statusWrapper, index);/g' "$PATCH_SRC_DIR/Statement.cpp"
 
 # 5. BackupManager.h - Aggregate initialization fix
 perl -i -pe 's/\.emplace_back\(value, ([^)]+)\)/.push_back({value, $1})/g' "$PATCH_SRC_DIR/BackupManager.h"
