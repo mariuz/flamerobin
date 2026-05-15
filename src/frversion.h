597cba93f43a3165613ba73c03f908ced3d19597
Popa Adrian Marius <mapopa@gmail.com>
2026-05-15 19:13:01 +0300

    release new version

--- src/frversion.h
+++ src/frversion.h
@@ -1,6 +1,6 @@
 #define FR_VERSION_MAJOR    26
 #define FR_VERSION_MINOR    5
-#define FR_VERSION_RLS      11
+#define FR_VERSION_RLS      12
 
 
 // if this file can't be found you need to run the update-revision-info script
