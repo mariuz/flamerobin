1c27940814d03e89e3744ab4cb8a0bbfeb43b6e2
Popa Marius Adrian <mapopa@gmail.com>
2026-06-20 00:03:31 +0300

    Bump version to 26.6.16 and update changelog

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ bool startCrashpad()
     std::map<std::string, std::string> annotations;
     annotations["format"] = "minidump";
     annotations["prod"] = "FlameRobin";
-    annotations["ver"] = "26.6.15";
+    annotations["ver"] = "26.6.16";
 
     std::vector<std::string> arguments;
     arguments.push_back("--no-rate-limit");
